12577d64f627fbe2a2305798a354b9d5fc88bb59
Adrien GIVRY <adrien@givry.pro>
2017-12-03 19:34:49 -0500

    Creating enemy projectiles and updating cake turretmode (Need to be timed with cooldown)

--- include/Projectile.h
+++ include/Projectile.h
@@ -5,12 +5,14 @@ class Projectile : public Actor
 {
 private:
 	const std::string __PROJECTILE_TEXTURE = "Projectile";
+	const std::string __ENEMY_PROJECTILE_TEXTURE = "Sugar";
+
 	const float __PROJECTILE_SPEED = 800;
 	const float __PROJECTILE_DAMAGES = 50;
 	const float __PROJECTILE_HITRATE = 100;
 
 public:
-	explicit Projectile(SharedContext* p_sharedContext, const Vector2D<float> p_direction, const float p_x = 0, const float p_y = 0);
+	explicit Projectile(SharedContext* p_sharedContext, const Vector2D<float> p_direction, const float p_x = 0, const float p_y = 0, const bool p_friendly = true);
 	~Projectile();
 
 	void Update(const sf::Time& l_time) override;
@@ -19,7 +21,12 @@ public:
 	void MultiplySpeed(const float p_value);
 	void MultiplyHitrate(const float p_value);
 
+	void SetDamages(const float p_value);
+	void SetSpeed(const float p_speed);
+
 private:
+	bool m_friendly;
+
 	float m_damages;
 	float m_hitrate;
 };
--- src/SpawnPoint.cpp
+++ src/SpawnPoint.cpp
@@ -46,7 +46,7 @@ SpawnPoint::SpawnPoint(SharedContext * p_sharedContext) :
 	while (!spawnIsCorrect && tries < 9999);
 
 	m_position = newPos;
-	m_type = SpawnerType::LOLLIPOP_SPAWNER;
+	m_type = SpawnerType::CAKEMONSTER_SPAWNER;
 	//m_type = static_cast<SpawnerType>(Utils::randomgen(0, 4));
 	switch (m_type)
 	{
